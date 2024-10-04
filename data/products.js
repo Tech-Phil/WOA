46f2cdd618042e80b2cc5791500a55c7246f6569
PhilJay Igtanloc <techpulis2021.04@gmail.com>
2024-10-04 23:26:49 +0800

    starting products details

---
+++ data/products.js
@@ -0,0 +1,15 @@
+const products = [{
+    id: "1",
+    image: "data/artImage"
+    name: "pods #4444",
+    size: "42 W x 28H",
+    creator: "Cobalt Delmigon",
+    price: 2200,
+    country: "Spain",
+    type: [
+        "portait",
+        "canvas" ,
+        "oil paint"
+    ]
+},
+];
